99e119147ee2756644e6165fd98d9eaf36c2b181
afincato <andre.not.me@gmail.com>
2016-08-05 10:18:55 +0200

    updated script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-	var path_mp3 = 'assets/audio/mp3',
+	var path_mp3 = 'assets/audio/mp3/',
   files = ['test.mp3', 'test2.mp3', 'test3.mp3'],
   i = Math.floor(Math.random()*files.length);
   var url = (path_mp3+files[i]);
   $(".audio-track--mp3").attr('src', url);
 
-  var_path_ogg path = 'assets/audio/ogg',
+  var path_ogg = 'assets/audio/ogg/',
   files = ['test.ogg', 'test2.ogg', 'test3.ogg'],
   i = Math.floor(Math.random()*files.length);
-  var url = (path+files[i]);
+  var url = (path_ogg+files[i]);
   $(".audio-track--ogg").attr('src', url);
 
 });
