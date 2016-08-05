7907c10cf46ae36e4e1c2320e055f7946f4cab67
afincato <andre.not.me@gmail.com>
2016-08-05 10:16:53 +0200

    updated script to randomise src selection

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
-	var path = '/path-to-files/',
-  files = ['1.php', '2.php', '3.php', '4.php', '5.php', '6.php'],
+	var path_mp3 = 'assets/audio/mp3',
+  files = ['test.mp3', 'test2.mp3', 'test3.mp3'],
+  i = Math.floor(Math.random()*files.length);
+  var url = (path_mp3+files[i]);
+  $(".audio-track--mp3").attr('src', url);
+
+  var_path_ogg path = 'assets/audio/ogg',
+  files = ['test.ogg', 'test2.ogg', 'test3.ogg'],
   i = Math.floor(Math.random()*files.length);
   var url = (path+files[i]);
-  $("#my-div").load(url);
+  $(".audio-track--ogg").attr('src', url);
 
 });
