b164c193f2dc200db1fa77abbe879c1961223ff3
afincato <andre.not.me@gmail.com>
2016-08-05 11:01:38 +0200

    updated script to load and bind audio track with gofish! button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+	$('.audio-track').trigger('load');
+
+	$('.button--fish').click(function() {
+    $('.audio-track').trigger('play');  
+  });
+
+  // randomly load a track
 	var path_mp3 = 'assets/audio/mp3/',
   files = ['test.mp3', 'test2.mp3', 'test3.mp3'],
   i = Math.floor(Math.random()*files.length);
