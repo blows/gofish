b364196ab0bec3de0b1a27eaec37e678007b6d28
afincato <andre.not.me@gmail.com>
2016-08-05 11:37:51 +0200

    updated class names

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-	$('.audio-track').trigger('load');
+	$('.audio--track').trigger('load');
 
 	$('.button--fish').click(function() {
-    $('.audio-track').trigger('play');  
+    $('.audio--track').trigger('play');  
   });
 
   // randomly load a track
@@ -11,12 +11,12 @@ $(document).ready(function() {
   files = ['test.mp3', 'test2.mp3', 'test3.mp3'],
   i = Math.floor(Math.random()*files.length);
   var url = (path_mp3+files[i]);
-  $(".audio-track--mp3").attr('src', url);
+  $(".audio--track__mp3").attr('src', url);
 
   var path_ogg = 'assets/audio/ogg/',
   files = ['test.ogg', 'test2.ogg', 'test3.ogg'],
   i = Math.floor(Math.random()*files.length);
   var url = (path_ogg+files[i]);
-  $(".audio-track--ogg").attr('src', url);
+  $(".audio--track__ogg").attr('src', url);
 
 });
